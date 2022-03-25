3cb5424a4ea949ffbbd8f18fa50304bc02eb4c11
Chase <chase.lindsay1@gmail.com>
2022-03-24 23:21:51 -0600

    added themecontext file and necessary states

---
+++ src/components/ThemeContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+function ThemeProvider(props) {
+	const [ theme, setTheme ] = useState('light');
+	const [ location, setLocation ] = useState('london');
+	function toggleTheme() {
+		if (theme === 'light') {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	}
+	const value = {
+		theme,
+		location,
+		toggleTheme
+	};
+	return <ThemeContext.Provider value={value}>{props.children}</ThemeContext.Provider>;
+}
